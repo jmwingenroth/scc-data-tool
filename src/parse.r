37ca580f719b42147cae70e966411177cab24049
jmwingenroth <jwing421@gmail.com>
2025-07-19 12:00:31 -0400

    Fix flipped negative values

--- src/parse.r
+++ src/parse.r
@@ -7,6 +7,6 @@ web_ready %>%
     filter(XSC == "RFF-SPs", XAD == "None") %>%
     separate_wider_delim(CO2:N2O, ",", names_sep = "_") %>%
     select(XDR, YEA, matches("[1-3]$")) %>%
-    mutate(across(CO2_1:N2O_3, \(x) str_extract(x, "(\\d*\\.?\\d+)"))) %>%
+    mutate(across(CO2_1:N2O_3, \(x) str_extract(x, "(-*\\d*\\.?\\d+)"))) %>%
     mutate(across(CO2_1:N2O_3, as.numeric)) %>%
     write_csv("rff_defaults.csv")
